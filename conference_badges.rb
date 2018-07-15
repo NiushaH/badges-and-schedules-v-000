a0dc36a6f5add9680d23a53c23f9ee9ae232caf3
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-15 05:00:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,8 +11,17 @@ def assign_rooms(attendees)
 room_assignments = attendees.each_with_index.collect { |badgename, roomnum| p "Hello, #{badgename}! You'll be assigned to room #{roomnum + 1}!" }
 end 
 
+<<<<<<< HEAD
 def printer(attendees)
   need_to_print = batch_badge_creator(attendees), assign_rooms(attendees)
   badges.each{ |conf_speaker| puts conf_speaker } 
   room_assignments.each{ |conf_speaker| puts conf_speaker }
 end
+=======
+def printer
+  batch_badge_creator(attendees)
+  assign_rooms(attendees)
+end
+
+# printer
+>>>>>>> 5b2cce344a2a904b2e2eeba5e95dbf7e962733cb
